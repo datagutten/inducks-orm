4167dfa001823036d64e15b2ea2dbea50f02d439
datagutten <datagutten@datagutten.net>
2023-07-23 12:57:03 +0200

    Add database config

--- bootstrap.php
+++ bootstrap.php
@@ -12,15 +12,29 @@ function InducksORMBootstrap(): EntityManager
     );
 
 // configuring the database connection
-    $config_db = require __DIR__ . '/config_db.php';
-    $connection = DriverManager::getConnection([
-        'driver' => 'pdo_mysql',
-        'user' => $config_db['db_user'],
-        'password' => $config_db['db_password'],
-        'dbname' => $config_db['db_name'],
-        'host' => $config_db['db_host'],
-        'charset' => 'utf8',
-    ], $config);
+    if (file_exists(__DIR__ . '/config_db.php'))
+    {
+        $config_db = require __DIR__ . '/config_db.php';
+        $connection = DriverManager::getConnection([
+            'driver' => 'pdo_mysql',
+            'user' => $config_db['db_user'],
+            'password' => $config_db['db_password'],
+            'dbname' => $config_db['db_name'],
+            'host' => $config_db['db_host'],
+            'charset' => 'utf8',
+        ], $config);
+    }
+    else
+    {
+        $connection = DriverManager::getConnection([
+            'driver' => 'pdo_mysql',
+            'user' => getenv('DB_USER'),
+            'password' => getenv('DB_PASSWORD'),
+            'dbname' => getenv('DB_DATABASE'),
+            'host' => getenv('DB_HOST'),
+            'charset' => 'utf8',
+        ], $config);
+    }
     $connection->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
 // obtaining the entity manager
     return new EntityManager($connection, $config);
