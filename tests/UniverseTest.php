e2007574281d9fd0f714fb2015b7856da7dbd654
datagutten <datagutten@datagutten.net>
2023-07-29 19:15:38 +0200

    Add more tests for universe

--- tests/UniverseTest.php
+++ tests/UniverseTest.php
@@ -3,7 +3,9 @@
 use datagutten\InducksORM\models\Character;
 use datagutten\InducksORM\models\Universe;
 use datagutten\InducksORM\models\UniverseCharacter;
+use datagutten\InducksORM\models\UniverseName;
 use PHPUnit\Framework\TestCase;
+
 
 class UniverseTest extends TestCase
 {
@@ -14,5 +16,18 @@ class UniverseTest extends TestCase
         $this->assertInstanceOf(UniverseCharacter::class, $character);
         $this->assertInstanceOf(Character::class, $character->getCharacter());
         $this->assertEquals($universe, $character->getUniverse());
+    }
+
+    public function testLocalizedName()
+    {
+        $universe = InducksORMBootstrap()->find(Universe::class, 'Riverside Rovers');
+        $name = $universe->getLocalizedName('no');
+        $this->assertEquals('Elvebreddkameratene', $name);
+    }
+
+    public function testName()
+    {
+        $names = InducksORMBootstrap()->getRepository(UniverseName::class)->findBy(['universecode' => 'Riverside Rovers']);
+        $this->assertNotEmpty($names);
     }
 }
