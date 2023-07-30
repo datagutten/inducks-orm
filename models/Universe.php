6a95e0053f34671cd1cdec1214d8047d5e67d543
datagutten <datagutten@datagutten.net>
2023-07-30 15:31:26 +0200

    Add relation from Universe to UniverseName

--- models/Universe.php
+++ models/Universe.php
@@ -2,9 +2,11 @@
 
 namespace datagutten\InducksORM\models;
 
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\PersistentCollection;
+
 
 #[ORM\Table(name: 'inducks_universe')]
 #[ORM\Entity(readOnly: true)]
@@ -18,6 +20,9 @@ class Universe
 
     #[ORM\OneToMany(mappedBy: 'universe', targetEntity: UniverseCharacter::class)]
     private PersistentCollection $characters;
+
+    #[ORM\OneToMany(mappedBy: 'universe', targetEntity: UniverseName::class)]
+    private PersistentCollection $names;
 
     public function getUniverseCode(): string
     {
@@ -35,5 +40,28 @@ class Universe
     public function getCharacters(): PersistentCollection
     {
         return $this->characters;
+    }
+
+    /**
+     * @return PersistentCollection<int, UniverseName>
+     */
+    public function getNames(): PersistentCollection
+    {
+        return $this->names;
+    }
+
+    public function getLocalizedName($languageCode): ?string
+    {
+        $criteria = Criteria::create()->where(Criteria::expr()->eq('languagecode', $languageCode));
+        $matches = $this->names->matching($criteria);
+        if (!empty($matches->count()))
+            return $matches->first()->getUniverseName();
+        else
+            return null;
+    }
+
+    public function __toString()
+    {
+        return $this->universecode;
     }
 }
